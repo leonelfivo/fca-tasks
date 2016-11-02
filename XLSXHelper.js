a28ff12e294bdc7552f2a02db11c9362bd29dbeb
Fabricio Silva de Souza <fabricio.collins@gmail.com>
2016-11-02 02:07:29 +0000

    XLSXHelper.js edited online with Bitbucket

--- XLSXHelper.js
+++ XLSXHelper.js
@@ -66,7 +66,7 @@ exports.generate = function(stream) {
 	    		attachments: []
 	    	};
 
-        	//MailHelper.send(mailParams);
+        	MailHelper.send(mailParams);
 	        MailHelper.send(mailNotificationParams);		    
 
 	    });
